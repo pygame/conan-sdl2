52517e5291e00292a94c89b47af1fb165ed45fee
René Dudfield <renesd@gmail.com>
2020-12-23 13:07:36 +0100

    2.0.14 release tar ball

--- conanfile.py
+++ conanfile.py
@@ -186,8 +186,9 @@ class SDL2Conan(ConanFile):
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
-        # extracted_dir = "SDL2-" + self.version
-        extracted_dir = "SDL-0ac2d6bb6c99"
+        extracted_dir = "SDL2-" + self.version
+        # this is for a tip download
+        # extracted_dir = "SDL-0ac2d6bb6c99"
         os.rename(extracted_dir, self._source_subfolder)
 
         if "patches" in self.conan_data and self.version in self.conan_data["patches"]:
